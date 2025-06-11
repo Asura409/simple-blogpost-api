54afec8b069f87f5bb475a941a68290e8b28d4d0
Asura409 <145164179+Asura409@users.noreply.github.com>
2025-06-11 16:47:07 +0100

    route/endpoint handlers

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+    "github.com/gorilla/mux"
 )
+var route = mux.NewRouter()
 
-func printHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
-}
 func main() {
-	http.HandleFunc("/hello", printHello)
-	http.ListenAndServe(":8080", nil)
+	route.HandleFunc("/posts", add_posts).Methods("POST")
+    route.HandleFunc("/posts/{id}", edit_posts).Methods("PUT")
+    route.HandleFunc("/posts/{id}", delete_post).Methods("DELETE")
+    route.HandleFunc("/posts", get_posts).Methods("GET")
+    route.HandleFunc("/posts/{id}", get_post_by_id).Methods("GET")
+	http.ListenAndServe(":8080", route)
 }
